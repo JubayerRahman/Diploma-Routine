a388481130871cdb0a97f43ca5f573209393dd3d
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2022-03-13 16:30:39 +0600

    Add files via upload

---
+++ funtion.js
@@ -0,0 +1,45 @@
+
+var submit = document.getElementById("Cst4th").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="0001.jpg";
+    
+
+})
+var submit2 = document.getElementById("Cst2nd").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="CST2nd.jpg";
+
+})
+var submit2 = document.getElementById("Cst6th").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="CST6th.jpg";
+
+})
+var submit2 = document.getElementById("Cst1st").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="Sorry The system don't have any routine for your exam.png";
+
+
+})
+var submit2 = document.getElementById("Cst3rd").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="Sorry The system don't have any routine for your exam.png";
+
+})
+var submit2 = document.getElementById("Cst5th").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="Sorry The system don't have any routine for your exam.png";
+
+
+})
+var submit2 = document.getElementById("Cst7th").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="Sorry The system don't have any routine for your exam.png";
+
+})
+var submit2 = document.getElementById("Cst8th").addEventListener("click", function(){
+    var img = document.querySelector('img');
+    img.src="Sorry The system don't have any routine for your exam.png";
+
+
+})
